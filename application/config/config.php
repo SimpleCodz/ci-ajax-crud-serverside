f4876d64f8f81104e3e79f395ba730c6caa1431f
SimpleCodz <46218591+SimpleCodz@users.noreply.github.com>
2019-01-03 14:55:22 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-apps/ci-ajax-crud-serverside';
+$config['base_url'] = 'http://localhost/project_name';
 
 /*
 |--------------------------------------------------------------------------
